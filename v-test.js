7e561bc8553272f91a5a50ede19b6f2421784394
Hsiu <77731262+Hsiu2023@users.noreply.github.com>
2023-06-02 23:22:35 +0800

    Update v-test.js

--- v-test.js
+++ v-test.js
@@ -3,11 +3,9 @@ const http = require('http')
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World\n')
+  res.end('Hello, World!\n')
 })
 
-const port = process.env.PORT || 3000
-
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+server.listen(8000, () => {
+  console.log(`Server running at http://localhost:8000/`)
 })
