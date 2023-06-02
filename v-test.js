5e4c89316a584b58acbe69a1d8861920c78b6d6f
Hsiu <77731262+Hsiu2023@users.noreply.github.com>
2023-06-02 23:26:19 +0800

    Update v-test.js

--- v-test.js
+++ v-test.js
@@ -1,11 +1,26 @@
-const http = require('http')
+// const http = require('http')
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello, World!\n')
-})
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200
+//   res.setHeader('Content-Type', 'text/plain')
+//   res.end('Hello, World!\n')
+// })
 
-server.listen(8000, () => {
-  console.log(`Server running at http://localhost:8000/`)
-})
+// server.listen(8000, () => {
+//   console.log(`Server running at http://localhost:8000/`)
+// })
+
+
+
+const express = require("express");
+const app = express();
+
+// 設定路由
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// 設定伺服器聆聽埠 8000
+app.listen(8000, () => {
+  console.log('Server running on port 8000');
+});
